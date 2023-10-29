d87f2c00726609e1ef35f4b2a614fb0db3b076c5
Eddy Lujan <eddylujann@gmail.com>
2023-10-29 10:17:41 +0100

    fix

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -3,6 +3,19 @@ import bcrypt from 'bcryptjs';
 import token from "../services/token";
 
 export default {
+    register_admin: async(req, res) => {
+        try {
+            req.body.password = await bcrypt.hash(req.body.password, 10);
+            const user = await models.User.create(req.body);
+            res.status(200).json(user);
+        } catch (error) {
+            res.status(500).send({
+                message: "debbug: UserController register - OCURRIÓ UN PROBLEMA"
+            });
+            console.log(error);
+        }
+    },
+
     register: async(req, res) => {
         try {
             req.body.password = await bcrypt.hash(req.body.password, 10);
@@ -54,6 +67,47 @@ export default {
             console.log("debbug:-------------");
         }
     },
+    login_admin: async(req, res) => {
+        try {
+            const user = await models.User.findOne({email: req.body.email, state:1, rol:"admin"});
+            if (user) {
+                // SI ESTÁ REGISTRADO EN EL SISTEMA
+                let compare = await bcrypt.compare(req.body.password, user.password);
+                if (compare) {
+                    let tokenT = await token.encode(user._id, user.rol, user.email);
+                    const USER_FRONTED = {
+                        token: tokenT,
+                        user: {
+                            name: user.name,
+                            email: user.email,
+                            surname: user.surname,
+                            avatar: user.avatar,
+                            rol: user.rol,
+                        }
+                    }
+                    res.status(200).json({
+                        USER_FRONTED:USER_FRONTED
+                    });
+                } else {
+                    res.status(500).send({
+                        message: "EL USUARIO NO EXISTE"
+                    });
+                }
+            } else {
+                res.status(500).send({
+                    message: "EL USUARIO NO EXISTE"
+                });
+            }
+        } catch (error) {
+            res.status(500).send({
+                message: "debbug: UserController login - OCURRIÓ UN PROBLEMA"
+            });
+            console.log("debbug:-------------");
+            console.log(error);
+            console.log("debbug:-------------");
+        }
+    },
+
     update: async(req, res) => {
         try {
             if (req.files) {
