dc30d292cfae01b98118116d8b464dac3731f29b
Eddy Lujan <eddylujann@gmail.com>
2024-01-07 22:43:15 +0100

    HomeController Modal

--- controllers/HomeController.js
+++ controllers/HomeController.js
@@ -15,20 +15,29 @@ export default {
             });
 
             let BestProducts = await models.Product.find({state:2}).sort({"createdAt": -1});
-            BestProducts = BestProducts.map((product) => {
-                return resources.Product.product_list(product);
-            });
+
+            var ObjectBestProducts = [];
+            for (const Product of BestProducts) {
+                let variedades = await models.Variedad.find({product: Product._id});
+                ObjectBestProducts.push(resources.Product.product_list(Product,variedades));
+            }
 
             let OurProducts = await models.Product.find({state:2}).sort({"createdAt": 1});
-            OurProducts = OurProducts.map((product) => {
-                return resources.Product.product_list(product);
-            });
+            var ObjectOurProducts = [];
+            for (const Product of OurProducts) {
+                let variedades = await models.Variedad.find({product: Product._id});
+                ObjectOurProducts.push(resources.Product.product_list(Product,variedades));
+            }
+            /*OurProducts = OurProducts.map(async(product) => {
+                let variedades = await models.Variedad.find({product: product._id});
+                return resources.Product.product_list(product,variedades);
+            });*/
 
             res.status(200).json({
                 sliders: Sliders,
                 categories: Categories,
-                bes_products: BestProducts,
-                our_products: OurProducts,
+                bes_products: ObjectBestProducts,
+                our_products: ObjectOurProducts,
             });
 
         } catch (error) {
