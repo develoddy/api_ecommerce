e80c2ec9ad24d2e3a92e7099de880351847b2d46
Eddy Lujan <eddylujann@gmail.com>
2024-01-08 23:25:37 +0100

    home products

--- controllers/HomeController.js
+++ controllers/HomeController.js
@@ -4,6 +4,9 @@ import resources from "../resources";
 export default {
     list: async(req, res) => {
         try {
+
+            var TIME_NOW = req.query.TIME_NOW;
+
             let Sliders = await models.Slider.find({state:1});
             Sliders = Sliders.map((slider) => {
                 return resources.Slider.slider_list(slider);
@@ -28,16 +31,38 @@ export default {
                 let variedades = await models.Variedad.find({product: Product._id});
                 ObjectOurProducts.push(resources.Product.product_list(Product,variedades));
             }
-            /*OurProducts = OurProducts.map(async(product) => {
-                let variedades = await models.Variedad.find({product: product._id});
-                return resources.Product.product_list(product,variedades);
-            });*/
+            //OurProducts = OurProducts.map(async(product) => {
+            //    let variedades = await models.Variedad.find({product: product._id});
+            //    return resources.Product.product_list(product,variedades);
+            //});
+
+            let FlashSale = await models.Discount.findOne({
+                type_campaign: 2,
+                start_date_num: {$lte: TIME_NOW}, // <=
+                end_date_num: {$gte: TIME_NOW},  // >=
+            });
+
+            let ProductList = [];
+
+            if (FlashSale) {
+                for (const product of FlashSale.products) {
+                    var ObjectT = await models.Product.findById({_id: product._id});
+                    let variedades = await models.Variedad.find({product: product._id});
+                    ProductList.push(resources.Product.product_list(ObjectT,variedades));
+                }
+            } else {
+                console.log("No hay FlashSale");
+                FlashSale = null;
+                ProductList = [];
+            }
 
             res.status(200).json({
                 sliders: Sliders,
                 categories: Categories,
                 bes_products: ObjectBestProducts,
                 our_products: ObjectOurProducts,
+                FlashSale: FlashSale,
+                campaign_products: ProductList,
             });
 
         } catch (error) {
